a1a370335e6424d112a5cf070370a65d842b47c7
SwimmingSushi <76238065+SwimmingSushi@users.noreply.github.com>
2025-03-30 15:41:35 -0700

    Initial Commit

---
+++ IB/IBApp.swift
@@ -0,0 +1,17 @@
+//
+//  IBApp.swift
+//  IB
+//
+//  Created by Andrew Zhou on 3/30/25.
+//
+
+import SwiftUI
+
+@main
+struct IBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ IBTests/IBTests.swift
@@ -0,0 +1,16 @@
+//
+//  IBTests.swift
+//  IBTests
+//
+//  Created by Andrew Zhou on 3/30/25.
+//
+
+import Testing
+
+struct IBTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
